1930575e997b166bee894864484b1a3bd9255e6a
T.F. <45820630+inokoe@users.noreply.github.com>
2025-02-05 22:09:09 +0800

    Dev: 🍎 域名测试

--- api/cdn/image.ts
+++ api/cdn/image.ts
@@ -10,7 +10,7 @@ export async function GET(request: Request) {
   }
 
   // 构建新的 URL
-  const url = `https://i.pximg.net/${proxyPath}`
+  const url = `https://i.pixiv.re/${proxyPath}`
 
   console.log(url)
 
