a4618c94ef777f5ee708654fcb5243f0c62fa7af
T.F. <45820630+inokoe@users.noreply.github.com>
2025-02-05 18:29:24 +0800

    Dev: 0.3.10 - 🍆 插画展示页面

--- src/pages/Pixiv/Show/index.tsx
+++ src/pages/Pixiv/Show/index.tsx
@@ -2,16 +2,15 @@ import PixivCarousel from '@/components/Pixiv/Carousel'
 import useShowData from '@/hooks/useShowData'
 import Skeleton from '@/components/ui/skeleton'
 import { DataContext } from './context'
-
+import PixivShowDetail from '@/components/Pixiv/ShowDetail'
 const PixivShowBody = () => {
   return (
-    <div className='flex flex-row w-full h-full rounded-lg gap-4 overflow-hidden'>
-      <div className='w-2/3 h-full flex-shrink-0 rounded-lg flex justify-center items-center overflow-hidden border border-gray-200 dark:border-gray-800 shadow-[0_8px_30px_rgb(0,0,0,0.12)] dark:shadow-[0_8px_30px_rgba(255,255,255,0.05)] backdrop-blur-sm'>
+    <div className='flex flex-col xl:flex-row w-full h-full rounded-lg gap-4 overflow-auto scrollbar-hide p-4'>
+      <div className='w-full h-4/5 xl:h-full xl:w-2/3 flex-shrink-0 rounded-lg flex justify-center items-center overflow-hidden border border-gray-200 bg-slate-100 dark:border-slate-600 shadow-[0_8px_30px_rgb(0,0,0,0.12)] dark:shadow-[0_8px_30px_rgba(255,255,255,0.05)] backdrop-blur-sm'>
         <PixivCarousel />
       </div>
-      <div className='w-1/3 h-full flex-shrink-0 rounded-lg bg-slate-100'>
-        <div className='w-full h-1/6 flex-shrink-0 '>111</div>
-        <div className='w-full h-5/6 flex-shrink-0 '>222</div>
+      <div className='w-full h-auto xl:w-1/3 xl:h-full flex-shrink-0 rounded-lg bg-slate-100 dark:bg-zinc-800 backdrop-blur-sm'>
+        <PixivShowDetail />
       </div>
     </div>
   )
